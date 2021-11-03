c3964b6f0fe793d41c1378c3a05fd08173fd6d64
TheAlexGo <alex280702@mail.ru>
2021-11-03 11:49:32 +0300

    Добавьте файлы проекта.

---
+++ WordGuessGame/Program.cs
@@ -0,0 +1,34 @@
+﻿using WordGuessLibrary;
+using static System.Console;
+
+namespace WordGuessGame{
+	internal class Program{
+		static void Main(string[] args){
+			OutputEncoding = InputEncoding = System.Text.Encoding.Unicode;
+			string word, description;
+			do{
+				Write("Введите слово: ");
+				word = ReadLine();
+				Write("Опишите слово: ");
+				description = ReadLine();
+			}
+			while(!WordGuess.IsWord(word) || string.IsNullOrEmpty(description));
+			string wordGuessed = new string('*', word.Length);
+			do{
+				char character;
+				do{
+					Clear();
+					WriteLine("Слово: " + wordGuessed);
+					WriteLine("Описание: " + description);
+					Write("Введите букву: ");
+					string input = ReadLine();
+					character = (input.Length == 1 && char.IsLetter(input[0])) ? input[0] : '*';
+				}
+				while(character == '*');
+				wordGuessed = WordGuess.ShowCharacters(word, wordGuessed, WordGuess.FindCharacterOccurences(word, character));
+			}
+			while(word != wordGuessed);
+			WriteLine($"Слово \"{wordGuessed}\" отгадано!");
+		}
+	}
+}
---
+++ WordGuessLibrary/WordGuess.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace WordGuessLibrary{
+	public static class WordGuess{
+		public static int[] FindCharacterOccurences(string word, char character){
+			if(string.IsNullOrEmpty(word))
+				return new int[]{};
+			word = word.ToLower();
+			character = char.ToLower(character);
+			List<int> found = new List<int>();
+			for(int i = 0; i < word.Length; i++)
+				if(word[i] == character)
+					found.Add(i);
+			return found.ToArray();
+		}
+
+		public static string ShowCharacters(string word, string wordGuessed, int[] positions){
+			if(word.Length != wordGuessed.Length || word == wordGuessed || positions == null || positions.Length == 0)
+				return wordGuessed;
+			StringBuilder wordTemporary = new StringBuilder(wordGuessed);
+			for(int i = 0; i < positions.Length; i++)
+                wordTemporary[positions[i]] = word[positions[i]];
+			return wordTemporary.ToString();
+		}
+
+		public static bool IsWord(string word){
+			if(string.IsNullOrEmpty(word))
+				return false;
+			for(int i = 0; i < word.Length; i++)
+				if(!char.IsLetter(word[i]))
+					return false;
+			return true;
+		}
+	}
+}
